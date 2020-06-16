b5f6f16330e571604f94d48d91748107c9bce59e
AndyLu <andyludeveloper@gmail.com>
2020-06-17 00:36:22 +0800

    prog. 2-3 test exception

--- src/main/java/LogAnalyzer.java
+++ src/main/java/LogAnalyzer.java
@@ -1,5 +1,8 @@
 public class LogAnalyzer {
     public boolean isValidLogFileName(String fileName){
+        if(fileName==null){
+            throw new IllegalArgumentException("filename has to be provided");
+        }
         if(fileName.toUpperCase().endsWith(".SLF")){
             return true;
         }else{
--- src/test/java/LogAnalyzerTest.java
+++ src/test/java/LogAnalyzerTest.java
@@ -1,21 +1,31 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class LogAnalyzerTest {
-
+    private final LogAnalyzer analyzer = new LogAnalyzer();
     @Test
     public void isValidLogFileName_GoodExtensionLowercase_ReturnsTrue() {
-        LogAnalyzer analyzer = new LogAnalyzer();
         boolean result = analyzer.isValidLogFileName("filewithgoodextension.slf");
         assertTrue(result);
     }
 
     @Test
     public void isValidLogFileName_GoodExtensionUppercase_ReturnsTrue() {
-        LogAnalyzer analyzer = new LogAnalyzer();
         boolean result = analyzer.isValidLogFileName("filewithgoodextension.SLF");
         assertTrue(result);
+    }
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void isValidLogFileName_EmptyFileName_Throws() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("filename has to be provided");
+        analyzer.isValidLogFileName(null);
     }
 
 }
