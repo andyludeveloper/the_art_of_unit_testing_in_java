b5b61a4488af0e9119faa08437041b27a2b12914
AndyLu <andyludeveloper@gmail.com>
2020-06-20 13:54:29 +0800

    refactor: Initial SUT instance from class.

--- src/test/java/MemCalculatorTest.java
+++ src/test/java/MemCalculatorTest.java
@@ -3,17 +3,16 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MemCalculatorTest {
+    private MemCalculator SUT = new MemCalculator();
 
     @Test
     public void Sum_ByDefault_ReturnZero() {
-        MemCalculator SUT = new MemCalculator();
         int result = SUT.sum();
         assertEquals(0, result);
     }
 
     @Test
     public void Add_WhenCalled_ChangesSum(){
-        MemCalculator SUT = new MemCalculator();
         SUT.add(1);
         int result = SUT.sum();
         assertEquals(1, result);
