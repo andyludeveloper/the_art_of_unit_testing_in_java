c8f6d21f0b7de90d1c76b7021da73656617c6b8c
AndyLu <andyludeveloper@gmail.com>
2020-06-20 13:51:59 +0800

    prog 2-8 test add() method

--- src/test/java/MemCalculatorTest.java
+++ src/test/java/MemCalculatorTest.java
@@ -10,4 +10,12 @@ public class MemCalculatorTest {
         int result = SUT.sum();
         assertEquals(0, result);
     }
+
+    @Test
+    public void Add_WhenCalled_ChangesSum(){
+        MemCalculator SUT = new MemCalculator();
+        SUT.add(1);
+        int result = SUT.sum();
+        assertEquals(1, result);
+    }
 }
